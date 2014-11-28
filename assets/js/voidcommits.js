42aadf71fa7bf3a0079b407d575ff241f977619b
Enno Boland <g@s01.de>
2014-11-28 10:39:28 +0100

    voidcommits: fix wrong escaping, show full commit messages in title attribute

--- assets/js/voidcommits.js
+++ assets/js/voidcommits.js
@@ -1,14 +1,15 @@
 function voidcommits(response) {
 	var html = "", commit, i;
 	function esc(str) {
-		return str.replace('"', '&quot;').
-			replace('<', '&lt;').
-			replace('>', '&gt;');
+		return str.replace(/"/g, '&quot;').
+			replace(/</g, '&lt;').
+			replace(/>/g, '&gt;');
 	}
 	for(i = 0; i < response.data.length; i++) {
 		commit = response.data[i];
-		html += '<li><a href="'+esc(commit.html_url)+'">'+
-			esc(commit.commit.message.split('\n')[0]) + "</a></li>"
+		html += '<li><a href="' + esc(commit.html_url) + '" title="' +
+			esc(commit.commit.message) + '">' +
+			esc(commit.commit.message.split('\n')[0]) + "</a></li>";
 	}
 	document.write(html);
 }
