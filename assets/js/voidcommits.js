6a42de4f6b84c8af699e18f0e0061819414cb5ef
classabbyamp <void@placeviolette.net>
2023-07-04 16:08:20 -0400

    index.html: replace twitter feed with void-packages pull feed
    the twitter feed is broken (thanks elon) and redundant (it only includes
    news posts from this site these days)
    
    fixes #208

--- assets/js/voidcommits.js
+++ assets/js/voidcommits.js
@@ -13,3 +13,20 @@ function voidcommits(response) {
 	}
 	document.write(html);
 }
+
+function voidpulls(response) {
+	var html = "", commit, i;
+	function esc(str) {
+		return str.replace(/"/g, '&quot;').
+			replace(/</g, '&lt;').
+			replace(/>/g, '&gt;');
+	}
+	for(i = 0; i < response.data.length; i++) {
+		pull = response.data[i];
+		html += '<li><a href="' + esc(pull.html_url) + '" title="' +
+			esc(pull.title) + '">' +
+			'#' + pull.number + ': ' +
+			esc(pull.title.split('\n')[0]) + "</a></li>";
+	}
+	document.write(html);
+}
